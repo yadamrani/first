dac3312de8a7d7e33805a2e323c4a2f6debd7e8b
EC2 Default User <ec2-user@ip-172-31-43-191.ap-northeast-1.compute.internal>
2024-11-12 08:08:23 +0000

    nn

--- hey.java
+++
@@ -1,8 +0,0 @@
-this is first
-
-
-this is second change 
-
-
-
-  this  is 3rd change from another server
--- payment.java
+++
@@ -1,2 +0,0 @@
- hey this file is created from branchname1
-changes from another devoleper
